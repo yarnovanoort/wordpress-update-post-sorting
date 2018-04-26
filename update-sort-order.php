120cedec8598f423e004a0d74d2abf24aae464da
Yarno van Oort <yarnovanoort@gmail.com>
2018-04-26 10:26:24 +0200

    Added start file plugin

---
+++ update-sort-order.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Auto update sort order
+Description: Automate the update of the sort order of wordpress post
+Author: Yarno van Oort<ik@yarnovanoort.nl>
+Version: 1.0
+Author URI: https://www.yarnovanoort.nl
+Text Domain: update-sort-order
+*/
+
+if (!class_exists('update_sort_order')) {
+
+    class update_sort_order
+    {
+
+        public function __construct()
+        {
+
+        }
+    }
+}
+
